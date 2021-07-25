b9e84221fa48feccebc4bd9e3d62171de76e77ec
Benjamin Ran <beni@pinecone.io>
2021-07-24 22:09:34 -0700

    add back missing pinecone.__init__ imports of pinecone.legacy.index

--- pinecone/__init__.py
+++ pinecone/__init__.py
@@ -9,6 +9,7 @@ from pinecone.legacy.specs.traffic_router import TrafficRouter  # noqa
 from pinecone.utils.sentry import sentry_decorator as sentry
 from .constants import Config
 from pinecone.legacy.manage import create_index, delete_index, describe_index, list_indexes, ResourceDescription
+from pinecone.legacy.index import Index, UpsertResult, DeleteResult, QueryResult, FetchResult, InfoResult
 
 __all__ = [
     "init",
