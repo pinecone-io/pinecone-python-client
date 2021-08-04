63f93466e0372d42896551a4449dc3ada86d8c55
Benjamin Ran <beni@pinecone.io>
2021-08-03 23:09:22 -0700

    update test_sanity to use cli params and correct openapi usage

--- tests/integration/test_sanity.py
+++ tests/integration/test_sanity.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 
@@ -5,11 +6,11 @@ import pinecone
 
 # use: export PINECONE_API_KEY=foobar; export PINECONE_PROJECT_NAME=beni; export PINECONE_ENVIROMENT=alpha; python test_sanity.py
 from pinecone.experimental.index_grpc import Index
-from pinecone.protos.vector_service_pb2 import UpsertRequest, QueryRequest, DenseVector, AnonymousVector
+from pinecone.protos.vector_service_pb2 import UpsertRequest, QueryRequest, DenseVector, AnonymousVector, DeleteRequest
 
 
-def manual_test_grpc():
-    index = Index('test-1')
+def manual_test_grpc(args):
+    index = Index(args.index_name)
     logging.info('got grpc upsert response: %s', index.Upsert(
         UpsertRequest(vectors=[
             DenseVector(id='A', values=[0.1, 0.2, 0.3]),
@@ -33,37 +34,29 @@ def manual_test_grpc():
     ))
 
 
-def manual_test_openapi():
-    index_name = 'test-1'
-    project_name = 'beni'
-    env = 'dev-benjaminran'
-    api_key = os.getenv('API_KEY')
-
+def manual_test_openapi(args):
     import pinecone.experimental.openapi
     from pinecone.experimental.openapi.api import vector_service_api
+    # import pinecone.experimental.openapi.exceptions
+    # import pinecone.experimental.openapi.configuration
     from pprint import pprint
-    configuration = pinecone.openapi.Configuration(
-        host=f"https://{index_name}-{project_name}.{env}.svc.pinecone.io"
-    )
-    # Configure API key authorization: ApiKeyAuth
-    configuration.api_key['Api-Key'] = api_key
 
-    with pinecone.openapi.ApiClient(configuration) as api_client:
+    configuration = pinecone.experimental.openapi.Configuration(
+        host=f"https://{args.index_name}-{args.project_name}.svc.{args.pinecone_env}.pinecone.io",
+        api_key={'ApiKeyAuth': args.api_key}
+    )
+    # configuration.verify_ssl = False
+    # configuration.proxy = 'http://localhost:8111'
+
+    with pinecone.experimental.openapi.ApiClient(configuration) as api_client:
         api_instance = vector_service_api.VectorServiceApi(api_client)
-        request_id = "requestId_example"  # str | Unique id of the request. (optional)
-        ids = [
-            "ids_example",
-        ]
-        delete_all = True
-        namespace = "namespace_example"
         try:
-            # The Delete operation deletes a vector by id.
             api_response = api_instance.vector_service_delete(
-                request_id=request_id, ids=ids, delete_all=delete_all,
-                namespace=namespace)
+                request_id='1234', ids=['A', 'B'], delete_all=False,
+                namespace='ns1')
             pprint(api_response)
-        except pinecone.openapi.ApiException as e:
-            print("Exception when calling VectorServiceApi->vector_service_delete: %s\n" % e)
+        except pinecone.experimental.openapi.OpenApiException as e:
+            logging.exception("Exception when calling VectorServiceApi->vector_service_delete", e)
 
 
 def manual_test_all_legacy():
@@ -95,7 +88,18 @@ def manual_test_all_legacy():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api-key', default='701868b2-2f96-442e-97fd-4430dafe728d')
+    parser.add_argument('--project-name', default='sharechat')
+    parser.add_argument('--pinecone-env', default='sharechat-production')
+    parser.add_argument('--index-name')
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO)
-    test_grpc()
-    test_openapi()
-    # test_all_legacy()
+    logging.info('invoked with args: %s', args)
+    pinecone.init(project_name=args.project_name, api_key=args.api_key, environment=args.pinecone_env)
+    logging.info('config: %s', pinecone.Config._config._asdict())
+
+    # manual_test_grpc(args)
+    manual_test_openapi(args)
+    # manual_test_all_legacy()
