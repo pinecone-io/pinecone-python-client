e9455e9ffd8aebd9ebc8a6c29ab16d0ff8a4798a
Benjamin Ran <beni@pinecone.io>
2021-09-01 20:37:26 -0700

    Add missed exceptions to pinecone.exceptions

--- pinecone/exceptions.py
+++ pinecone/exceptions.py
@@ -1,6 +1,7 @@
 from .core.openapi.exceptions import OpenApiException, ApiAttributeError, ApiTypeError, ApiValueError, \
-    ApiKeyError, ApiException
+    ApiKeyError, ApiException, NotFoundException, UnauthorizedException, ForbiddenException, ServiceException
 
 __all__ = [
-    "OpenApiException", "ApiAttributeError", "ApiTypeError", "ApiValueError", "ApiKeyError", "ApiException"
+    "OpenApiException", "ApiAttributeError", "ApiTypeError", "ApiValueError", "ApiKeyError", "ApiException",
+    "NotFoundException", "UnauthorizedException", "ForbiddenException", "ServiceException"
 ]
