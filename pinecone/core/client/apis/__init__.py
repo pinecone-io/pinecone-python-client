2631634bfbc73f59e4a0e11fac953f2b0a1aa9b2
rajat08 <rajat.tripathi.08@gmail.com>
2021-09-21 10:19:47 -0400

    adding back the license

--- pinecone/core/client/apis/__init__.py
+++ pinecone/core/client/apis/__init__.py
@@ -1,3 +1,6 @@
+#
+# Copyright (c) 2020-2021 Pinecone Systems Inc. All right reserved.
+#
 
 # flake8: noqa
 
--- pinecone/core/client/model/rpc_status.py
+++ pinecone/core/client/model/rpc_status.py
@@ -1,3 +1,7 @@
+#
+# Copyright (c) 2020-2021 Pinecone Systems Inc. All right reserved.
+#
+
 """
     Pinecone JSON API
 
