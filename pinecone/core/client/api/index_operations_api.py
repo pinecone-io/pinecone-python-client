bcac7ac122a3250a34c8a2e9c5a00d1da74d8885
pineconemachine <ops@pinecone.io>
2022-07-24 20:00:38 +0000

    Code update

--- pinecone/core/client/api/index_operations_api.py
+++ pinecone/core/client/api/index_operations_api.py
@@ -428,6 +428,124 @@ class IndexOperationsApi(object):
             callable=__describe_index
         )
 
+        def __list_collections(
+            self,
+            **kwargs
+        ):
+            """list_collections  # noqa: E501
+
+            This operation returns a list of your Pinecone collections.  # noqa: E501
+            This method makes a synchronous HTTP request by default. To make an
+            asynchronous HTTP request, please pass async_req=True
+
+            >>> thread = api.list_collections(async_req=True)
+            >>> result = thread.get()
+
+
+            Keyword Args:
+                _return_http_data_only (bool): response data without head status
+                    code and headers. Default is True.
+                _preload_content (bool): if False, the urllib3.HTTPResponse object
+                    will be returned without reading/decoding response data.
+                    Default is True.
+                _request_timeout (int/float/tuple): timeout setting for this request. If
+                    one number provided, it will be total request timeout. It can also
+                    be a pair (tuple) of (connection, read) timeouts.
+                    Default is None.
+                _check_input_type (bool): specifies if type checking
+                    should be done one the data sent to the server.
+                    Default is True.
+                _check_return_type (bool): specifies if type checking
+                    should be done one the data received from the server.
+                    Default is True.
+                _host_index (int/None): specifies the index of the server
+                    that we want to use.
+                    Default is read from the configuration.
+                async_req (bool): execute request asynchronously
+
+            Returns:
+                [str]
+                    If the method is called asynchronously, returns the request
+                    thread.
+            """
+            kwargs['async_req'] = kwargs.get(
+                'async_req', False
+            )
+            kwargs['_return_http_data_only'] = kwargs.get(
+                '_return_http_data_only', True
+            )
+            kwargs['_preload_content'] = kwargs.get(
+                '_preload_content', True
+            )
+            kwargs['_request_timeout'] = kwargs.get(
+                '_request_timeout', None
+            )
+            kwargs['_check_input_type'] = kwargs.get(
+                '_check_input_type', True
+            )
+            kwargs['_check_return_type'] = kwargs.get(
+                '_check_return_type', True
+            )
+            kwargs['_host_index'] = kwargs.get('_host_index')
+            return self.call_with_http_info(**kwargs)
+
+        self.list_collections = _Endpoint(
+            settings={
+                'response_type': ([str],),
+                'auth': [
+                    'ApiKeyAuth'
+                ],
+                'endpoint_path': '/collections',
+                'operation_id': 'list_collections',
+                'http_method': 'GET',
+                'servers': [
+                    {
+                        'url': "https://controller.{environment}.pinecone.io",
+                        'description': "No description provided",
+                        'variables': {
+                            'environment': {
+                                'description': "No description provided",
+                                'default_value': "unknown",
+                                }
+                            }
+                    },
+                ]
+            },
+            params_map={
+                'all': [
+                ],
+                'required': [],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                },
+                'attribute_map': {
+                },
+                'location_map': {
+                },
+                'collection_format_map': {
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json; charset=utf-8'
+                ],
+                'content_type': [],
+            },
+            api_client=api_client,
+            callable=__list_collections
+        )
+
         def __list_indexes(
             self,
             **kwargs
