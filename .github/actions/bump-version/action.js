82a807d5149ab852b68b7da19f63c85f8057cab5
Jen Hamon <jhamon@pinecone.io>
2023-06-02 19:32:45 -0400

    Add action for bumping version numbers

---
+++ .github/actions/bump-version/action.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+
+function bumpVersion(currentVersion, bumpType, prerelease) {
+  let newVersion = calculateNewVersion(currentVersion, bumpType, prerelease);
+
+  if (prerelease !== undefined && prerelease !== '') {
+    newVersion = `${newVersion}.${prerelease}`;
+  }
+  core.setOutput('previous_version', currentVersion)
+  core.setOutput('previous_version_tag', `v${currentVersion}`)
+  core.setOutput('version', newVersion);
+  core.setOutput('version_tag', `v${newVersion}`)
+
+  return newVersion;
+}
+
+function calculateNewVersion(currentVersion, bumpType, prerelease) {
+  const [major, minor, patch] = currentVersion.split('.');
+  let newVersion;
+
+  switch (bumpType) {
+    case 'major':
+      newVersion = `${parseInt(major) + 1}.0.0`;
+      break;
+    case 'minor':
+      newVersion = `${major}.${parseInt(minor) + 1}.0`;
+      break;
+    case 'patch':
+      newVersion = `${major}.${minor}.${parseInt(patch) + 1}`;
+      break;
+    default:
+      throw new Error(`Invalid bumpType: ${bumpType}`);
+  }
+
+  return newVersion;
+}
+
+module.exports = { bumpVersion }
