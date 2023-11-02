47cffce86fd980cd1f189221287c387c7ac4d752
Jen Hamon <jhamon@pinecone.io>
2023-11-01 20:25:59 -0400

    Remove numpy

--- pinecone/utils/__init__.py
+++ pinecone/utils/__init__.py
@@ -3,3 +3,4 @@ from .environment import get_version, get_environment
 from .user_agent import get_user_agent
 from .deprecation_notice import warn_deprecated
 from .fix_tuple_length import fix_tuple_length
+from .convert_to_list import convert_to_list
