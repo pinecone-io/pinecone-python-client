85e1b100fb59f2c96e3fd9bfd051e9261b206158
Jen Hamon <jhamon@pinecone.io>
2023-11-03 23:44:32 -0700

    Type fixes for config

--- pinecone/config/config.py
+++ pinecone/config/config.py
@@ -1,7 +1,6 @@
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 import os
 
-from pinecone.utils import check_kwargs
 from pinecone.exceptions import PineconeConfigurationError
 from pinecone.core.client.exceptions import ApiKeyError
 from pinecone.config.openapi import OpenApiConfigFactory
@@ -11,7 +10,7 @@ from pinecone.core.client.configuration import Configuration as OpenApiConfigura
 class ConfigBase(NamedTuple):
     api_key: str = ""
     host: str = ""
-    openapi_config: OpenApiConfiguration = None
+    openapi_config: Optional[OpenApiConfiguration] = None
 
 
 class Config:
@@ -33,28 +32,26 @@ class Config:
 
     def __init__(
         self,
-        api_key: str = None,
-        host: str = None,
-        openapi_config: OpenApiConfiguration = None,
+        api_key: Optional[str] = None,
+        host: Optional[str] = None,
+        openapi_config: Optional[OpenApiConfiguration] = None,
         **kwargs,
     ):
         api_key = api_key or kwargs.pop("api_key", None) or os.getenv("PINECONE_API_KEY")
         host = host or kwargs.pop("host", None)
+
+        if not api_key:
+            raise ApiKeyError("You haven't specified an Api-Key.")
+        if not self._config.host:
+            raise PineconeConfigurationError("You haven't specified a host.")
+
         openapi_config = (
             openapi_config
             or kwargs.pop("openapi_config", None)
             or OpenApiConfigFactory.build(api_key=api_key, host=host)
         )
 
-        check_kwargs(self.__init__, kwargs)
-        self._config = ConfigBase(api_key, host, openapi_config)
-        self.validate()
-
-    def validate(self):
-        if not self._config.api_key:
-            raise PineconeConfigurationError("You haven't specified an Api-Key.")
-        if not self._config.host:
-            raise PineconeConfigurationError("You haven't specified a host.")
+        self._config: ConfigBase = ConfigBase(api_key, host, openapi_config)
 
     @property
     def API_KEY(self):
