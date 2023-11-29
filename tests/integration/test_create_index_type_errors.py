0c7ae30fbf8245992a0771e442062878d0f936ff
Jen Hamon <jhamon@pinecone.io>
2023-11-29 17:56:50 -0500

    Fix syntax error

--- tests/integration/test_create_index_type_errors.py
+++ tests/integration/test_create_index_type_errors.py
@@ -1,10 +1,10 @@
 import pytest
-from pinecone import ApiTypeError
+from pinecone import PineconeApiTypeError
 
 class TestCreateIndexTypeErrorCases:
     def test_create_index_with_invalid_str_dimension(self, client, create_sl_index_params):
         create_sl_index_params['dimension'] = '10'
-        with pytest.raises(ApiTypeError):
+        with pytest.raises(PineconeApiTypeError):
             client.create_index(**create_sl_index_params)
 
     def test_create_index_with_missing_dimension(self, client, create_sl_index_params):
