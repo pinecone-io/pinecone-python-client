ca35deb7d9f2e782d7bb177da438f46118eaa66f
Jen Hamon <jhamon@pinecone.io>
2024-01-05 04:20:48 -0500

    Add back __version__ file

--- pinecone/utils/version.py
+++ pinecone/utils/version.py
@@ -1,16 +1,6 @@
-def get_version():
-    try:
-        with open("pyproject.toml", "r") as f:
-            lines = f.readlines()
-    except FileNotFoundError:
-        return "unknown"
+from pathlib import Path
 
-    version_line = next((line for line in lines if line.startswith("version")), None)
-    if version_line:
-        # Extract the version number from the line
-        version = version_line.split("=")[1].strip().strip('"')
-        return version
-    else:
-        return "unknown"
+def get_version():
+    return Path(__file__).parent.parent.joinpath("__version__").read_text().strip()
 
 __version__ = get_version()
