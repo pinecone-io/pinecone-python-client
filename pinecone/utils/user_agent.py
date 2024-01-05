8b469cb7b92725dc7532c29e59ecd194f84b9a66
Jen Hamon <jhamon@pinecone.io>
2024-01-05 14:34:04 -0500

    Resolve merge conflicts

--- pinecone/utils/user_agent.py
+++ pinecone/utils/user_agent.py
@@ -1,10 +1,9 @@
-import requests
 import urllib3
 
 from .version import __version__
 
 def get_user_agent():
-    client_id = f"python-client-{__version__}"
-    user_agent_details = {"requests": requests.__version__, "urllib3": urllib3.__version__}
+    client_id = f"python-client-{get_version()}"
+    user_agent_details = {"urllib3": urllib3.__version__}
     user_agent = "{} ({})".format(client_id, ", ".join([f"{k}:{v}" for k, v in user_agent_details.items()]))
     return user_agent
---
+++ tests/dependency/grpc/test_sanity.py
@@ -0,0 +1,50 @@
+import pytest
+import os
+import time
+from pinecone import ServerlessSpec
+from pinecone.grpc import PineconeGRPC
+
+@pytest.fixture
+def index_name():
+    return 'test-sanity-rest'
+
+@pytest.fixture
+def client():
+    return PineconeGRPC(api_key=os.environ.get('PINECONE_API_KEY'))
+
+@pytest.fixture(autouse=True)
+def cleanup(index_name, client):
+    yield
+    if index_name in client.list_indexes().names():
+        client.delete_index(name=index_name)
+
+class TestSanityRest:
+    def test_sanity(self, index_name, client):
+        if index_name not in client.list_indexes().names():
+            client.create_index(
+                name=index_name, 
+                dimension=2, 
+                spec=ServerlessSpec(cloud="aws", region="us-west-2")
+            )
+        else:
+            pytest.fail('Index ' + index_name + ' already exists')
+        
+        idx = client.Index(index_name)
+        idx.upsert(vectors=[
+            ('1', [1.0, 2.0]), 
+            ('2', [3.0, 4.0]),
+            ('3', [5.0, 6.0])
+        ])
+
+        # Wait for index freshness
+        time.sleep(30)
+
+        # Check the vector count reflects the upserted data
+        description = idx.describe_index_stats()
+        assert description.dimension == 2
+        assert description.total_vector_count == 3
+
+        # Query for results
+        query_results = idx.query(id='1', top_k=10, include_values=True)
+        assert query_results.matches[0].id == '1'
+        assert len(query_results.matches) == 3
