8cf42e0fd726ff55be680275d8b73a83686ef8ca
Jen Hamon <jhamon@pinecone.io>
2024-01-13 16:13:35 -0500

    Add cleanup step

---
+++ scripts/3.x/delete-all-indexes.py
@@ -0,0 +1,24 @@
+import random
+import string
+import os
+from pinecone import Pinecone
+
+def read_env_var(name):
+    value = os.environ.get(name)
+    if value is None:
+        raise Exception('Environment variable {} is not set'.format(name))
+    return value
+
+def main():
+    api_key = read_env_var('PINECONE_API_KEY')
+   
+    pc = Pinecone(api_key=api_key)
+
+    indexes = pc.list_indexes()
+    for index in indexes:
+        print(f'Deleting index {index.name}...')
+        pc.delete_index(index.name)
+
+
+if __name__ == '__main__':
+    main()
