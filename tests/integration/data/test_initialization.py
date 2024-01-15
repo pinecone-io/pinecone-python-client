7fe843a6a148b080e13f4282293917aae8cffa32
Jen Hamon <jhamon@pinecone.io>
2024-01-14 22:41:56 -0500

    Fix typo in test

--- tests/integration/data/test_initialization.py
+++ tests/integration/data/test_initialization.py
@@ -8,16 +8,16 @@ class TestIndexClientInitialization:
     def test_index_direct_host_with_https(self, client, index_host):
         if not index_host.startswith('https://'):
             index_host = 'https://' + index_host
-        index = client.Index(index_host)
+        index = client.Index(host=index_host)
         index.fetch(ids=['1', '2', '3'])
 
-    def test_index_direct_host_without_http(self, client, index_host):
+    def test_index_direct_host_without_https(self, client, index_host):
         if index_host.startswith('https://'):
             index_host = index_host[8:]
         index = client.Index(host=index_host)
         index.fetch(ids=['1', '2', '3'])
 
-    def test_index_by_name_positional(self, client, index_name):
+    def test_index_by_name_positional_only(self, client, index_name, index_host):
         index = client.Index(index_name)
         index.fetch(ids=['1', '2', '3'])
 
@@ -33,6 +33,6 @@ class TestIndexClientInitialization:
         index = client.Index(name=index_name, host=index_host)
         index.fetch(ids=['1', '2', '3'])
 
-    def test_raises_when_no_name_or_host(self, client):
+    def test_raises_when_no_name_or_host(self, client, index_host):
         with pytest.raises(ValueError):
             client.Index()
