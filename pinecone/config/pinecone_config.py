b42977627bfeaeb29d5fbf1dfc8eedc22e9059c1
Leon Cheung <leon.c@pinecone.io>
2024-02-05 21:37:06 +0000

    init commit

--- pinecone/config/pinecone_config.py
+++ pinecone/config/pinecone_config.py
@@ -1,6 +1,10 @@
 from typing import Optional, Dict
+import logging
+import json
 import os
 from .config import ConfigBuilder, Config
+
+logger = logging.getLogger(__name__)
 
 DEFAULT_CONTROLLER_HOST = "https://api.pinecone.io"
 
@@ -9,4 +13,11 @@ class PineconeConfig():
     @staticmethod
     def build(api_key: Optional[str] = None, host: Optional[str] = None, additional_headers: Optional[Dict[str, str]] = {},  **kwargs) -> Config:
         host = host or kwargs.get("host") or os.getenv("PINECONE_CONTROLLER_HOST") or DEFAULT_CONTROLLER_HOST
+        try:
+          headers_json = os.getenv("PINECONE_ADDITIONAL_HEADERS")
+          headers = json.loads(headers_json)
+          additional_headers = additional_headers or headers
+        except json.decoder.JSONDecodeError as e:
+          logger.warn(f'Ignoring PINECONE_ADDITIONAL_HEADERS: {}', e)
+
         return ConfigBuilder.build(api_key=api_key, host=host, additional_headers=additional_headers, **kwargs)
