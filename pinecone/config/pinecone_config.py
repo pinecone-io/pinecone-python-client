989cbe7c234b9b085b708cee5f5b32b67b1ca6df
Leon Cheung <leon.c@pinecone.io>
2024-02-06 16:58:11 +0000

    fix none json issue

--- pinecone/config/pinecone_config.py
+++ pinecone/config/pinecone_config.py
@@ -13,11 +13,12 @@ class PineconeConfig():
     @staticmethod
     def build(api_key: Optional[str] = None, host: Optional[str] = None, additional_headers: Optional[Dict[str, str]] = {},  **kwargs) -> Config:
         host = host or kwargs.get("host") or os.getenv("PINECONE_CONTROLLER_HOST") or DEFAULT_CONTROLLER_HOST
-        try:
-          headers_json = os.getenv("PINECONE_ADDITIONAL_HEADERS")
-          headers = json.loads(headers_json)
-          additional_headers = additional_headers or headers
-        except json.decoder.JSONDecodeError as e:
-          logger.warn(f'Ignoring PINECONE_ADDITIONAL_HEADERS: {e}')
+        headers_json = os.getenv("PINECONE_ADDITIONAL_HEADERS")
+        if headers_json:
+          try:
+            headers = json.loads(headers_json)
+            additional_headers = additional_headers or headers
+          except json.decoder.JSONDecodeError as e:
+            logger.warn(f'Ignoring PINECONE_ADDITIONAL_HEADERS: {e}')
 
         return ConfigBuilder.build(api_key=api_key, host=host, additional_headers=additional_headers, **kwargs)
