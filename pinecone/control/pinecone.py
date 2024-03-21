90e92d79c6c56a1dc53fea38be2959dad7df82b8
Silas Smith <silas@pinecone.io>
2024-03-21 06:56:18 -0700

    Set source_integration on data plane requests

--- pinecone/control/pinecone.py
+++ pinecone/control/pinecone.py
@@ -633,5 +633,7 @@ class Pinecone:
             api_key=api_key,
             pool_threads=pt,
             openapi_config=openapi_config,
+            source_integration_name=self.config.source_integration_name,
+            source_integration_version=self.config.source_integration_version,
             **kwargs
         )
