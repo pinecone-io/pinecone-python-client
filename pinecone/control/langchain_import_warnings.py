00d46b5723512cf5d6ac6dc10962b90eac715c00
Jen Hamon <jhamon@pinecone.io>
2024-05-13 14:31:37 -0400

    Add better error messages for from_texts and from_documents

---
+++ pinecone/control/langchain_import_warnings.py
@@ -0,0 +1,8 @@
+from pinecone.utils import docslinks
+
+KB_ARTICLE = docslinks['LANGCHAIN_IMPORT_KB_ARTICLE']
+
+def _build_langchain_attribute_error_message(method_name: str):
+    return f"""{method_name} is not a top-level attribute of the Pinecone class provided by pinecone's official python package developed at https://github.com/pinecone-io/pinecone-python-client. You may have a name collision with an export from another dependency in your project that wraps Pinecone functionality and exports a similarly named class. Please refer to the following knowledge base article for more information: {KB_ARTICLE}
+"""
+
---
+++ pinecone/utils/docslinks.py
@@ -0,0 +1,3 @@
+docslinks = {
+    'LANGCHAIN_IMPORT_KB_ARTICLE': "https://docs.pinecone.io/troubleshooting/pinecone-attribute-errors-with-langchain"
+}
