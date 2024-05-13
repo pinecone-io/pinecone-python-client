b071d86657ae4c6deba327a50f7fedfc2f79a941
Jen Hamon <jhamon@pinecone.io>
2024-05-13 14:43:58 -0400

    Make a variable for repo location url

--- pinecone/utils/docslinks.py
+++ pinecone/utils/docslinks.py
@@ -1,3 +1,4 @@
 docslinks = {
-    'LANGCHAIN_IMPORT_KB_ARTICLE': "https://docs.pinecone.io/troubleshooting/pinecone-attribute-errors-with-langchain"
+    'GITHUB_REPO': 'https://github.com/pinecone-io/pinecone-python-client',
+    'LANGCHAIN_IMPORT_KB_ARTICLE': 'https://docs.pinecone.io/troubleshooting/pinecone-attribute-errors-with-langchain'
 }
