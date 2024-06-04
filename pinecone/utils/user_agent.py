b9b19ae7ae4a03de031b8aab432101314485b91b
Jen Hamon <jhamon@pinecone.io>
2024-06-04 16:27:49 -0400

    Update comment

--- pinecone/utils/user_agent.py
+++ pinecone/utils/user_agent.py
@@ -7,7 +7,7 @@ import re
 def _build_source_tag_field(source_tag):
     # normalize source tag
     # 1. Lowercase
-    # 2. Limit charset to [a-z0-9_ ]
+    # 2. Limit charset to [a-z0-9_ :]
     # 3. Trim left/right whitespace
     # 4. Condense multiple spaces to one, and replace with underscore
     tag = source_tag.lower()
