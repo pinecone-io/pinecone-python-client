17df1b7e08b855557b05d7041096df06ccbd390d
Jen Hamon <jhamon@pinecone.io>
2024-06-04 11:39:00 -0400

    Remove unused constants

--- pinecone/utils/constants.py
+++ pinecone/utils/constants.py
@@ -8,20 +8,6 @@ MAX_MSG_SIZE = 128 * 1024 * 1024
 MAX_ID_LENGTH = int(os.getenv("PINECONE_MAX_ID_LENGTH", default="64"))
 
 REQUEST_ID: str = "request_id"
-CLIENT_VERSION_HEADER = "X-Pinecone-Client-Version"
-
-
-class NodeType(str, enum.Enum):
-    STANDARD = "STANDARD"
-    COMPUTE = "COMPUTE"
-    MEMORY = "MEMORY"
-    STANDARD2X = "STANDARD2X"
-    COMPUTE2X = "COMPUTE2X"
-    MEMORY2X = "MEMORY2X"
-    STANDARD4X = "STANDARD4X"
-    COMPUTE4X = "COMPUTE4X"
-    MEMORY4X = "MEMORY4X"
-
 
 CLIENT_VERSION = __version__
 CLIENT_ID = f"python-client-{CLIENT_VERSION}"
