076c88c5127e15158a999887814230b43c4ce40e
gdj0nes <gareth@pinecone.io>
2024-06-12 21:06:32 -0400

    update regex and api version in test

--- tests/unit/utils/test_setup_openapi_client.py
+++ tests/unit/utils/test_setup_openapi_client.py
@@ -1,9 +1,12 @@
-import pytest
 import re
+
+import pytest
+
 from pinecone.config import ConfigBuilder
 from pinecone.core.client.api.manage_indexes_api import ManageIndexesApi
 from pinecone.core.client.api_client import ApiClient
 from pinecone.utils.setup_openapi_client import setup_openapi_client, build_plugin_setup_client
+
 
 class TestSetupOpenAPIClient():
     def test_setup_openapi_client(self):
@@ -15,7 +18,7 @@ class TestSetupOpenAPIClient():
         assert openapi_config.host == "https://my-controller-host"
 
         control_plane_client = setup_openapi_client(ApiClient, ManageIndexesApi, config=config, openapi_config=openapi_config, pool_threads=2)
-        user_agent_regex = re.compile(r"python-client-\d+\.\d+\.\d+ \(urllib3\:\d+\.\d+\.\d+\)")
+        user_agent_regex = re.compile(r"python-client-\\d+\\.\\d+\\.\\d+([a-z]+\\d+)? \\(urllib3\\:\\d+\\.\\d+\\.\\d+\\)")
         assert re.match(user_agent_regex, control_plane_client.api_client.user_agent)
         assert re.match(user_agent_regex, control_plane_client.api_client.default_headers['User-Agent'])
 
@@ -27,11 +30,11 @@ class TestSetupOpenAPIClient():
         openapi_config = ConfigBuilder.build_openapi_config(config)
         assert openapi_config.host == "https://my-controller-host"
 
-        control_plane_client = setup_openapi_client(ApiClient, ManageIndexesApi, config=config, openapi_config=openapi_config, pool_threads=2, api_version="2024-04")
-        user_agent_regex = re.compile(r"python-client-\d+\.\d+\.\d+ \(urllib3\:\d+\.\d+\.\d+\)")
+        control_plane_client = setup_openapi_client(ApiClient, ManageIndexesApi, config=config, openapi_config=openapi_config, pool_threads=2, api_version="2024-07")
+        user_agent_regex = re.compile(r"python-client-\\d+\\.\\d+\\.\\d+([a-z]+\\d+)? \\(urllib3\\:\\d+\\.\\d+\\.\\d+\\)")
         assert re.match(user_agent_regex, control_plane_client.api_client.user_agent)
         assert re.match(user_agent_regex, control_plane_client.api_client.default_headers['User-Agent'])
-        assert control_plane_client.api_client.default_headers['X-Pinecone-API-Version'] == "2024-04"
+        assert control_plane_client.api_client.default_headers['X-Pinecone-API-Version'] == "2024-07"
 
 
 class TestBuildPluginSetupClient():
@@ -75,9 +78,9 @@ class TestBuildPluginSetupClient():
         # purposes; in a real plugin, the class would be something else related 
         # to a new feature, but to test that this setup works I just need a FooApi 
         # class generated off the openapi spec.
-        plugin_api=ManageIndexesApi
+        plugin_api = ManageIndexesApi
         plugin_client = client_builder(
-            api_client_klass=ApiClient, 
+            api_client_klass=ApiClient,
             api_klass=plugin_api,
             api_version=plugin_api_version,
             host=plugin_host
@@ -103,7 +106,7 @@ class TestBuildPluginSetupClient():
 
         # Plugins need to be able to override the host (optionally)
         if plugin_host:
-           assert plugin_client.api_client.configuration._base_path == plugin_host
+            assert plugin_client.api_client.configuration._base_path == plugin_host
         else:
             # When plugin does not set a host, it should default to the host set in the core sdk
             assert plugin_client.api_client.configuration._base_path == "https://api.pinecone.io"
