416a0c615ac7cccade03c9de3a619935b476a7cb
Jen Hamon <jhamon@pinecone.io>
2024-06-26 14:08:53 -0400

    More cleanup

--- codegen/build-oas.sh
+++ codegen/build-oas.sh
@@ -3,6 +3,9 @@
 set -eux -o pipefail
 
 version='2024-07'
+modules=("control" "data")
+
+destination="pinecone/core"
 
 rm -rf build
 mkdir build
@@ -24,52 +27,57 @@ verify_spec_version() {
 		exit 1
 	fi
 
-	if [ ! -d "codegen/apis/_build/$version" ]; then
-		echo "Version $version does not exist in apis build dir"
+	verify_directory_exists "codegen/apis/_build/${version}"
+}
+
+verify_file_exists() {
+	local filename=$1
+	if [ ! -f "$filename" ]; then
+		echo "File does not exist at $filename"
 		exit 1
 	fi
 }
 
-verify_spec_file_exists() {
-	local oas_file=$1
-	if [ ! -f "$oas_file" ]; then
-		echo "Spec file does not exist at $oas_file"
+verify_directory_exists() {
+	local directory=$1
+	if [ ! -d "$directory" ]; then
+		echo "Directory does not exist at $directory"
 		exit 1
 	fi
 }
 
 generate_client() {
-	local oas_file=$1
-	local openapi_generator_config=$2
-	local template_dir="codegen/python-oas-templates/templates5.2.0"
+	local module_name=$1
 
-	if [ ! -f "$oas_file" ]; then
-		echo "Spec file does not exist at $oas_file"
-		exit 1
-	fi
-	if [ ! -d "$template_dir" ]; then
-		echo "Template directory does not exist at $template_dir"
-		exit 1
-	fi
+	oas_file="codegen/apis/_build/${version}/${module_name}_${version}.oas.yaml"
+	openapi_generator_config="codegen/openapi-config.${module_name}.json"
+	template_dir="codegen/python-oas-templates/templates5.2.0"
+	
+	verify_file_exists $oas_file
+	verify_file_exists $openapi_generator_config
+	verify_directory_exists $template_dir
 
 	# Cleanup previous build files
 	echo "Cleaning up previous build files"
 	rm -rf build
 
+	# Generate client module
 	docker run --rm -v $(pwd):/workspace openapitools/openapi-generator-cli:v5.2.0 generate \
 		--input-spec "/workspace/$oas_file" \
 		--generator-name python \
 		--config "/workspace/$openapi_generator_config" \
 		--output /workspace/build \
 		--template-dir "/workspace/$template_dir"
+
+	# Copy the generated module to the correct location
+	rm -rf "${destination}/${module_name}"
+	cp -r "build/pinecone/core/${module_name}" "${destination}/${module_name}"
 }
 
 extract_shared_classes() {
-	target_directory="pinecone/core/shared"
+	target_directory="${destination}/shared"
 	rm -rf $target_directory
 	mkdir -p $target_directory
-
-	modules=("control" "data")
 
 	# Define the list of shared source files
 	sharedFiles=(
@@ -80,18 +88,18 @@ extract_shared_classes() {
 		"rest"
 	)
 
-	source_directory="pinecone/core/${modules[0]}/client"
+	source_directory="${destination}/${modules[0]}/client"
 
 	# Loop through each file we want to share and copy it to the target directory
 	for file in "${sharedFiles[@]}"; do
-		cp "$source_directory/$file.py" "$target_directory"
+		cp "${source_directory}/${file}.py" "$target_directory"
 	done
 
 	# Cleanup shared files in each module
 	for module in "${modules[@]}"; do
-		source_directory="pinecone/core/$module/client"
+		source_directory="${destination}/${module}/client"
 		for file in "${sharedFiles[@]}"; do
-			rm "$source_directory/$file.py"
+			rm "${source_directory}/${file}.py"
 		done
 	done
 
@@ -102,7 +110,7 @@ extract_shared_classes() {
 	echo "All shared files have been copied to $target_directory."
 
 	# Adjust import paths in every file
-	find pinecone/core -name "*.py" | while IFS= read -r file; do
+	find "${destination}" -name "*.py" | while IFS= read -r file; do
 		sed -i '' 's/from \.\.model_utils/from pinecone\.core\.shared\.model_utils/g' "$file"
 		sed -i '' 's/from pinecone\.core\.control\.client import rest/from pinecone\.core\.shared import rest/g' "$file"
 
@@ -117,21 +125,9 @@ extract_shared_classes() {
 update_apis_repo
 verify_spec_version $version
 
-# Generate data plane client
-data_oas="codegen/apis/_build/$version/data_$version.oas.yaml"
-data_config="codegen/openapi-config.data.json"
-verify_spec_file_exists $data_oas
-generate_client $data_oas $data_config
-rm -rf pinecone/core/data
-cp -r build/pinecone/core/data pinecone/core/data
-
-# Generate control plane client
-control_oas="codegen/apis/_build/$version/control_$version.oas.yaml"
-control_config="codegen/openapi-config.control.json"
-verify_spec_file_exists $control_oas
-generate_client $control_oas $control_config
-rm -rf pinecone/core/control
-cp -r build/pinecone/core/control pinecone/core/control
+for module in "${modules[@]}"; do
+	generate_client $module
+done
 
 # Even though we want to generate multiple packages, we
 # don't want to duplicate every exception and utility class.
@@ -139,4 +135,4 @@ cp -r build/pinecone/core/control pinecone/core/control
 extract_shared_classes
 
 # Format generated files
-poetry run black pinecone/core
+poetry run black "${destination}"
