4fcaba350bfe59030759650d1d76344aa08f144c
Silas Smith <163026730+ssmith-pc@users.noreply.github.com>
2024-10-16 15:22:41 -0700

    add tests for exception import path

--- tests/integration/inference/test_rerank.py
+++ tests/integration/inference/test_rerank.py
@@ -1,5 +1,7 @@
+import pytest
 from pinecone import Pinecone
 from pinecone.grpc import PineconeGRPC
+from pinecone.exceptions import PineconeApiException
 
 
 class TestInferencePluginRerank:
@@ -10,7 +12,11 @@ class TestInferencePluginRerank:
         result = pc.inference.rerank(
             model=model,
             query="i love dogs",
-            documents=["dogs are pretty cool", "everyone loves dogs", "I'm a cat person"],
+            documents=[
+                "dogs are pretty cool",
+                "everyone loves dogs",
+                "I'm a cat person",
+            ],
             top_n=1,
             return_documents=True,
         )
@@ -28,7 +34,11 @@ class TestInferencePluginRerank:
         result = pc.inference.rerank(
             model=model,
             query="i love dogs",
-            documents=["dogs are pretty cool", "everyone loves dogs", "I'm a cat person"],
+            documents=[
+                "dogs are pretty cool",
+                "everyone loves dogs",
+                "I'm a cat person",
+            ],
             top_n=1,
             return_documents=True,
         )
@@ -38,3 +48,20 @@ class TestInferencePluginRerank:
         assert result.model == model
         assert isinstance(result.usage.rerank_units, int)
         assert result.usage.rerank_units == 1
+
+    def test_rerank_exception(self, api_key):
+        pc = Pinecone(api_key=api_key)
+        with pytest.raises(PineconeApiException) as e_info:
+            pc.inference.rerank(
+                model="DOES NOT EXIST",
+                query="i love dogs",
+                documents=[
+                    "dogs are pretty cool",
+                    "everyone loves dogs",
+                    "I'm a cat person",
+                ],
+                rank_fields=["custom-field"],
+                top_n=1,
+                return_documents=True,
+            )
+        assert e_info.value.status == 404
