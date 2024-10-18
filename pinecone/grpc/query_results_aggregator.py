c0173283f62fd07e889c5063c37f794d09ec6cbe
Jen Hamon <jhamon@pinecone.io>
2024-10-18 09:33:35 -0400

    More test cases for aggregator

--- pinecone/grpc/query_results_aggregator.py
+++ pinecone/grpc/query_results_aggregator.py
@@ -81,39 +81,85 @@ class CompositeQueryResults:
         )
 
 
+class QueryResultsAggregationEmptyResultsError(Exception):
+    def __init__(self, namespace: str):
+        super().__init__(
+            f"Cannot infer metric type from empty query results. Query result for namespace '{namespace}' is empty. Have you spelled the namespace name correctly?"
+        )
+
+
+class QueryResultsAggregregatorNotEnoughResultsError(Exception):
+    def __init__(self, top_k: int, num_results: int):
+        super().__init__(
+            f"Cannot interpret results without at least two matches. In order to aggregate results from multiple queries, top_k must be greater than 1 in order to correctly infer the similarity metric from scores. Expected at least {top_k} results but got {num_results}."
+        )
+
+
+class QueryResultsAggregatorInvalidTopKError(Exception):
+    def __init__(self, top_k: int):
+        super().__init__(f"Invalid top_k value {top_k}. top_k must be a positive integer.")
+
+
 class QueryResultsAggregator:
     def __init__(self, top_k: int):
+        if top_k < 1:
+            raise QueryResultsAggregatorInvalidTopKError(top_k)
         self.top_k = top_k
         self.usage_read_units = 0
         self.heap = []
         self.insertion_counter = 0
+        self.is_dotproduct = None
         self.read = False
+
+    def __is_dotproduct_index(self, matches):
+        # The interpretation of the score depends on the similar metric used.
+        # Unlike other index types, in indexes configured for dotproduct,
+        # a higher score is better. We have to infer this is the case by inspecting
+        # the order of the scores in the results.
+        for i in range(1, len(matches)):
+            if matches[i].get("score") > matches[i - 1].get("score"):  # Found an increase
+                return False
+        return True
 
     def add_results(self, results: QueryResponse):
         if self.read:
+            # This is mainly just to sanity check in test cases which get quite confusing
+            # if you read results twice due to the heap being emptied when constructing
+            # the ordered results.
             raise ValueError("Results have already been read. Cannot add more results.")
 
-        self.usage_read_units += results.get("usage", {}).get("readUnits", 0)
+        matches = results.get("matches", [])
         ns = results.get("namespace")
-        for match in results.get("matches", []):
-            self.insertion_counter += 1
-            score = match.get("score")
-            if len(self.heap) < self.top_k:
-                heapq.heappush(self.heap, (-score, -self.insertion_counter, match, ns))
-            else:
-                heapq.heappushpop(self.heap, (-score, -self.insertion_counter, match, ns))
+        self.usage_read_units += results.get("usage", {}).get("readUnits", 0)
+
+        if self.is_dotproduct is None:
+            if len(matches) == 0:
+                raise QueryResultsAggregationEmptyResultsError(ns)
+            if len(matches) == 1:
+                raise QueryResultsAggregregatorNotEnoughResultsError(self.top_k, len(matches))
+            self.is_dotproduct = self.__is_dotproduct_index(matches)
+
+        print("is_dotproduct:", self.is_dotproduct)
+        if self.is_dotproduct:
+            raise NotImplementedError("Dotproduct indexes are not yet supported.")
+        else:
+            for match in matches:
+                self.insertion_counter += 1
+                score = match.get("score")
+                if len(self.heap) < self.top_k:
+                    heapq.heappush(self.heap, (-score, -self.insertion_counter, match, ns))
+                else:
+                    heapq.heappushpop(self.heap, (-score, -self.insertion_counter, match, ns))
 
     def get_results(self) -> CompositeQueryResults:
         if self.read:
-            # This is mainly just to sanity check in test cases which get quite confusing
-            # if you read results twice due to the heap being emptied each time you read
-            # results into an ordered list.
-            raise ValueError("Results have already been read. Cannot read again.")
+            return self.final_results
         self.read = True
 
-        return CompositeQueryResults(
+        self.final_results = CompositeQueryResults(
             usage=Usage(read_units=self.usage_read_units),
             matches=[
                 ScoredVectorWithNamespace(heapq.heappop(self.heap)) for _ in range(len(self.heap))
             ][::-1],
         )
+        return self.final_results
