f74168fcfde139115f5adce98cf8e2a8b8996ad9
Jen Hamon <jhamon@pinecone.io>
2025-01-02 15:11:28 -0500

    Add tags test for pod index

--- tests/integration/control/pod/test_create_index.py
+++ tests/integration/control/pod/test_create_index.py
@@ -2,7 +2,7 @@ class TestCreateIndexPods:
     def test_create_with_optional_tags(self, client, create_index_params):
         index_name = create_index_params["name"]
         tags = {"foo": "FOO", "bar": "BAR"}
-        create_index_params["tags"] = create_index_params
+        create_index_params["tags"] = tags
 
         client.create_index(**create_index_params)
 
