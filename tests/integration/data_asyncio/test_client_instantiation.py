cc775e2c567cddf125e4acd387bd82de1c8dea1c
Jen Hamon <jhamon@pinecone.io>
2025-02-04 09:49:37 -0500

    Add new constructor method for async index

---
+++ tests/integration/data_asyncio/test_client_instantiation.py
@@ -0,0 +1,18 @@
+import pytest
+from pinecone import Pinecone
+from ..helpers import random_string, embedding_values
+
+
+@pytest.mark.asyncio
+async def test_instantiation_through_non_async_client(index_host, dimension):
+    asyncio_idx = Pinecone().IndexAsyncio(host=index_host)
+
+    def emb():
+        return embedding_values(dimension)
+
+    # Upsert with tuples
+    await asyncio_idx.upsert(
+        vectors=[("1", emb()), ("2", emb()), ("3", emb())], namespace=random_string(10)
+    )
+
+    await asyncio_idx.close()
