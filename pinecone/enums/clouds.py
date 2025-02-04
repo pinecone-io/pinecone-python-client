b31d1e4ed2d52c4517e187ae44c755c1108ce684
Jen Hamon <jhamon@pinecone.io>
2025-02-04 10:46:19 -0500

    Adjust docstrings

--- pinecone/enums/clouds.py
+++ pinecone/enums/clouds.py
@@ -2,7 +2,13 @@ from enum import Enum
 
 
 class CloudProvider(Enum):
-    """Cloud providers available for use with Pinecone serverless indexes"""
+    """Cloud providers available for use with Pinecone serverless indexes
+    
+    This list could expand or change over time as more cloud providers are supported.
+    Check the Pinecone documentation for the most up-to-date list of supported cloud 
+    providers. If you want to use a cloud provider that is not listed here, you can 
+    pass a string value directly without using this enum.
+    """
 
     AWS = "aws"
     GCP = "gcp"
@@ -10,7 +16,13 @@ class CloudProvider(Enum):
 
 
 class AwsRegion(Enum):
-    """AWS (Amazon Web Services) regions available for use with Pinecone serverless indexes"""
+    """AWS (Amazon Web Services) regions available for use with Pinecone serverless indexes
+    
+    This list could expand or change over time as more regions are supported.
+    Check the Pinecone documentation for the most up-to-date list of supported 
+    regions. If you want to use a region that is not listed here, you can 
+    pass a string value directly without using this enum.
+    """
 
     US_EAST_1 = "us-east-1"
     US_WEST_2 = "us-west-2"
@@ -18,13 +30,25 @@ class AwsRegion(Enum):
 
 
 class GcpRegion(Enum):
-    """GCP (Google Cloud Platform) regions available for use with Pinecone serverless indexes"""
+    """GCP (Google Cloud Platform) regions available for use with Pinecone serverless indexes
+    
+    This list could expand or change over time as more regions are supported.
+    Check the Pinecone documentation for the most up-to-date list of supported 
+    regions. If you want to use a region that is not listed here, you can 
+    pass a string value directly without using this enum.
+    """
 
     US_CENTRAL1 = "us-central1"
     EUROPE_WEST4 = "europe-west4"
 
 
 class AzureRegion(Enum):
-    """Azure regions available for use with Pinecone serverless indexes"""
+    """Azure regions available for use with Pinecone serverless indexes
+    
+    This list could expand or change over time as more regions are supported.
+    Check the Pinecone documentation for the most up-to-date list of supported 
+    regions. If you want to use a region that is not listed here, you can 
+    pass a string value directly without using this enum.
+    """
 
     EASTUS2 = "eastus2"
