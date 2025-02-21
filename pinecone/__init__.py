2e67c48e8927f83323f7e73cc812c5bd2441f0ae
Jen Hamon <jhamon@pinecone.io>
2025-02-21 13:29:46 -0500

    Adjust message

--- pinecone/__init__.py
+++ pinecone/__init__.py
@@ -3,5 +3,5 @@
 """
 
 raise Exception(
-    "The official Pinecone python package has been renamed from `pinecone-client` to `pinecone`. Please update your projects to use the `pinecone` package name. See the README at https://github.com/pinecone-io/pinecone-python-client for more information."
+    "The official Pinecone python package has been renamed from `pinecone-client` to `pinecone`. Please remove `pinecone-client` from your project dependencies and add `pinecone` instead. See the README at https://github.com/pinecone-io/pinecone-python-client for more information on using the python SDK."
 )
