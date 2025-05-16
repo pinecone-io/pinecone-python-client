d342f7adb406f4aad81129d6e888f3ca1716ea56
Jen Hamon <jhamon@pinecone.io>
2025-05-16 14:21:54 -0400

    Format printed representationi of backup

--- pinecone/db_control/models/backup_model.py
+++ pinecone/db_control/models/backup_model.py
@@ -7,14 +7,14 @@ class BackupModel:
     def __init__(self, backup: OpenAPIBackupModel):
         self._backup = backup
 
-    def __str__(self):
-        return str(self._backup)
-
     def __getattr__(self, attr):
         return getattr(self._backup, attr)
 
     def __getitem__(self, key):
         return self.__getattr__(key)
+
+    def __str__(self):
+        return self.__repr__()
 
     def __repr__(self):
         return json.dumps(self.to_dict(), indent=4, default=custom_serializer)
