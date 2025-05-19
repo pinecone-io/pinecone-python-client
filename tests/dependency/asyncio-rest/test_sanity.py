8086542393bed0521a7e468ee22c8a3d908cfa24
Jen Hamon <jhamon@pinecone.io>
2025-05-19 15:51:58 -0400

    Add logging in sanity tests

--- tests/dependency/asyncio-rest/test_sanity.py
+++ tests/dependency/asyncio-rest/test_sanity.py
@@ -3,6 +3,10 @@ import os
 import asyncio
 from pinecone import PineconeAsyncio
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture
 def index_name():
@@ -16,7 +20,7 @@ def index_name():
 class TestSanityRest:
     async def test_sanity(self, index_name):
         async with PineconeAsyncio() as pc:
-            print("Testing with index name: " + index_name)
+            logger.info("Testing with index name: " + index_name)
             assert index_name != ""
 
             # Verify index exists with expected properties
@@ -25,20 +29,26 @@ class TestSanityRest:
 
             description = await pc.describe_index(name=index_name)
             assert description.dimension == 2
+            logger.info("Index description: %s", description)
 
             idx = pc.IndexAsyncio(host=description.host)
-            await idx.upsert(vectors=[("1", [1.0, 2.0]), ("2", [3.0, 4.0]), ("3", [5.0, 6.0])])
+            resp = await idx.upsert(
+                vectors=[("1", [1.0, 2.0]), ("2", [3.0, 4.0]), ("3", [5.0, 6.0])]
+            )
+            logger.info("Upsert response: %s", resp)
 
             # Wait for index freshness
             await asyncio.sleep(30)
 
             # Check the vector count reflects some data has been upserted
             description = await idx.describe_index_stats()
+            logger.info("Index stats: %s", description)
             assert description.dimension == 2
             assert description.total_vector_count >= 3
 
             # Query for results
             query_results = await idx.query(id="1", top_k=10, include_values=True)
+            logger.info("Query results: %s", query_results)
             assert query_results.matches[0].id == "1"
             assert len(query_results.matches) == 3
 
@@ -49,10 +59,11 @@ class TestSanityRest:
             # Call an inference method, should not raise an exception
             from pinecone import EmbedModel
 
-            await pc.inference.embed(
+            resp = await pc.inference.embed(
                 model=EmbedModel.Multilingual_E5_Large,
                 inputs=["Hello, how are you?", "I am doing well, thank you for asking."],
                 parameters={"input_type": "passage", "truncate": "END"},
             )
+            logger.info("Embed response: %s", resp)
 
             await idx.close()
--- tests/dependency/grpc/test_sanity.py
+++ tests/dependency/grpc/test_sanity.py
@@ -2,6 +2,9 @@ import pytest
 import os
 import time
 from pinecone.grpc import PineconeGRPC
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -19,26 +22,30 @@ def client():
 
 class TestSanityRest:
     def test_sanity(self, index_name, client):
-        print("Testing with index name: " + index_name)
+        logger.info("Testing with index name: %s", index_name)
         assert index_name != ""
 
         # Verify index exists with expected properties
         assert index_name in client.list_indexes().names()
         description = client.describe_index(name=index_name)
         assert description.dimension == 2
+        logger.info("Index description: %s", description)
 
         idx = client.Index(index_name)
-        idx.upsert(vectors=[("1", [1.0, 2.0]), ("2", [3.0, 4.0]), ("3", [5.0, 6.0])])
+        resp = idx.upsert(vectors=[("1", [1.0, 2.0]), ("2", [3.0, 4.0]), ("3", [5.0, 6.0])])
+        logger.info("Upsert response: %s", resp)
 
         # Wait for index freshness
         time.sleep(30)
 
         # Check the vector count reflects some data has been upserted
         description = idx.describe_index_stats()
+        logger.info("Index stats: %s", description)
         assert description.dimension == 2
         assert description.total_vector_count >= 3
 
         # Query for results
         query_results = idx.query(id="1", top_k=10, include_values=True)
+        logger.info("Query results: %s", query_results)
         assert query_results.matches[0].id == "1"
         assert len(query_results.matches) == 3
