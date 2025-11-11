9150871f3f5d79546ddf3debb2216a531b33b2f3
Jen Hamon <jhamon@pinecone.io>
2025-11-11 12:47:28 -0500

    Fix asyncio tests

--- tests/integration/rest_asyncio/db/control/resources/index/test_configure_index_tags.py
+++ tests/integration/rest_asyncio/db/control/resources/index/test_configure_index_tags.py
@@ -4,20 +4,13 @@ from pinecone import PineconeAsyncio
 
 @pytest.mark.asyncio
 class TestIndexTags:
-    async def test_index_tags_none_by_default(self, ready_sl_index):
-        pc = PineconeAsyncio()
-
-        await pc.describe_index(name=ready_sl_index)
-        desc = await pc.describe_index(name=ready_sl_index)
-        assert desc.tags is None
-        await pc.close()
-
     async def test_add_index_tags(self, ready_sl_index):
         pc = PineconeAsyncio()
 
         await pc.configure_index(name=ready_sl_index, tags={"foo": "FOO", "bar": "BAR"})
         desc = await pc.describe_index(name=ready_sl_index)
-        assert desc.tags.to_dict() == {"foo": "FOO", "bar": "BAR"}
+        assert desc.tags.to_dict()["foo"] == "FOO"
+        assert desc.tags.to_dict()["bar"] == "BAR"
         await pc.close()
 
     async def test_remove_tags_by_setting_empty_value_for_key(self, ready_sl_index):
@@ -27,11 +20,13 @@ class TestIndexTags:
         await pc.configure_index(name=ready_sl_index, tags={})
 
         desc = await pc.describe_index(name=ready_sl_index)
-        assert desc.tags.to_dict() == {"foo": "FOO", "bar": "BAR"}
+        assert desc.tags.to_dict()["foo"] == "FOO"
+        assert desc.tags.to_dict()["bar"] == "BAR"
 
         await pc.configure_index(name=ready_sl_index, tags={"foo": ""})
         desc2 = await pc.describe_index(name=ready_sl_index)
-        assert desc2.tags.to_dict() == {"bar": "BAR"}
+        assert desc2.tags.to_dict()["bar"] == "BAR"
+        assert "foo" not in desc2.tags.to_dict()
         await pc.close()
 
     async def test_merge_new_tags_with_existing_tags(self, ready_sl_index):
@@ -40,7 +35,9 @@ class TestIndexTags:
         await pc.configure_index(name=ready_sl_index, tags={"foo": "FOO", "bar": "BAR"})
         await pc.configure_index(name=ready_sl_index, tags={"baz": "BAZ"})
         desc = await pc.describe_index(name=ready_sl_index)
-        assert desc.tags.to_dict() == {"foo": "FOO", "bar": "BAR", "baz": "BAZ"}
+        assert desc.tags.to_dict()["foo"] == "FOO"
+        assert desc.tags.to_dict()["bar"] == "BAR"
+        assert desc.tags.to_dict()["baz"] == "BAZ"
         await pc.close()
 
     @pytest.mark.skip(reason="Backend bug filed")
@@ -49,5 +46,6 @@ class TestIndexTags:
         await pc.configure_index(name=ready_sl_index, tags={"foo": "FOO", "bar": "BAR"})
         await pc.configure_index(name=ready_sl_index, tags={"foo": "", "bar": ""})
         desc = await pc.describe_index(name=ready_sl_index)
-        assert desc.tags is None
+        assert "foo" not in desc.tags.to_dict()
+        assert "bar" not in desc.tags.to_dict()
         await pc.close()
