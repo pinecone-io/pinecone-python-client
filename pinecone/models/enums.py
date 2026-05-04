01978a0e6c10200927728037f974a63e7c83893d
Jen Hamon <jhamon@pinecone.io>
2026-05-04 06:42:22 +0000

    feat: add support for new AWS and Azure serverless regions

--- pinecone/models/enums.py
+++ pinecone/models/enums.py
@@ -73,6 +73,8 @@ class AwsRegion(str, Enum):
     US_EAST_1 = "us-east-1"
     US_WEST_2 = "us-west-2"
     EU_WEST_1 = "eu-west-1"
+    EU_CENTRAL_1 = "eu-central-1"
+    AP_SOUTHEAST_1 = "ap-southeast-1"
 
 
 class GcpRegion(str, Enum):
@@ -86,6 +88,7 @@ class AzureRegion(str, Enum):
     """Azure regions supported for serverless indexes."""
 
     EASTUS2 = "eastus2"
+    GERMANYWESTCENTRAL = "germanywestcentral"
 
 
 class PodIndexEnvironment(str, Enum):
